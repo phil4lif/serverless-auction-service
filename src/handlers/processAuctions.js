feeda01de0f5236017b19dc5593557eea5d0e05a
phil cowan <philipjcowan1@gmail.com>
2021-09-09 18:22:58 -0700

    Adds function to actually close the auctions that need to be closed

--- src/handlers/processAuctions.js
+++ src/handlers/processAuctions.js
@@ -1,8 +1,15 @@
 import { getEndedAuctions } from '../lib/getEndedAuctions';
+import { closeAuction } from '../lib/closeAuction';
+import createError from 'http-errors';
 
 async function processAuctions(event, context) {
-  const auctionsToClose = await getEndedAuctions();
-  console.log(auctionsToClose)
+  try {
+    const auctionsToClose = await getEndedAuctions();
+    const closePromises = auctionsToClose.map(auction => closeAuction(auction))
+    await Promise.all(closePromises)
+  } catch (error) {
+    throw new createError.InternalServerError(error);
+  }
 }
 
 export const handler = processAuctions;
